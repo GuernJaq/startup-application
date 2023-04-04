ad1a9a97d114c987881e3d46feffae6abdffdac1
Jaquelyn Guernsey <guernjaq@gmail.com>
2023-04-04 15:53:08 -0600

    Added files/Refactor for startup service

---
+++ StartupPageFiles/database.js
@@ -0,0 +1,63 @@
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+
+const userName = process.env.MONGOUSER;
+const password = process.env.MONGOPASSWORD;
+const hostname = process.env.MONGOHOSTNAME;
+
+if (!userName) {
+  throw Error('Database not configured. Set environment variables');
+}
+
+const url = `mongodb+srv://${userName}:${password}@${hostname}`;
+
+const client = new MongoClient(url);
+const userCollection = client.db('startup').collection('user');
+const voteCollection = client.db('startup').collection('allVotes');
+const totVoteCollection = client.db('startup').collection('totVotes');
+
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function createUser(email, password) {
+  // Hash the password before we insert it into the database
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await userCollection.insertOne(user);
+
+  return user;
+}
+
+function addVote(vote) {
+  voteCollection.insertOne(vote);
+}
+
+function getAllVotes() {
+  const cursor = voteCollection.find();
+  return cursor.toArray();
+}
+
+function getVoteTotals(){
+    const cursor = totVoteCollection.find();
+    return cursor.toArray();
+}
+
+module.exports = {
+  getUser,
+  getUserByToken,
+  createUser,
+  addVote,
+  getAllVotes,
+  getVoteTotals,
+};
