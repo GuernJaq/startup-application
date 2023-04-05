5167f13d917bc373e57e90d652a32d2d4d102ca0
Jaquelyn Guernsey <guernjaq@gmail.com>
2023-04-04 19:27:13 -0600

    Added Websocket integration

--- StartupPageFiles/public/vote1.js
+++ StartupPageFiles/public/vote1.js
@@ -1,3 +1,8 @@
+const WrongVote = 'voteWrong';
+const RightVote = 'voteRight';
+const DupeVote = 'voteDupe';
+
+
 const btnDescriptions = [
     {name: 'Pikachu',correct:false},
     {name: 'Eevee',correct:false},
@@ -26,6 +31,8 @@ class UserVote{
         result.innerHTML = '' 
         const resBTN = document.getElementById('result-btn')
         resBTN.innerHTML = '' 
+
+        this.configureWebSocket();
     }
 
     getPlayerName(){
@@ -58,6 +65,8 @@ class UserVote{
         if(found){
             const alert = document.getElementById('alert')
             alert.innerHTML = '<div class="alert alert-warning" role="alert">You have already voted!</div>'
+            this.broadcastEvent(userName,DupeVote,{})
+
             delay(4000).then((e) => {alert.innerHTML =''})
         }else{
             votes.push(newVote);
@@ -76,9 +85,11 @@ class UserVote{
             if(correct){
                 const result = document.getElementById('result')
                 result.innerHTML = '<div class="alert alert-success" role="alert">You got it! Check results to see how others voted</div>'
+                this.broadcastEvent(userName,RightVote,{})
             }else{
                 const result = document.getElementById('result')
                 result.innerHTML = '<div class="alert alert-danger" role="alert">That isn\'t right! Check results to see how others voted</div>'
+                this.broadcastEvent(userName,WrongVote,{})
             }
         }
         const resBTN = document.getElementById('result-btn')
@@ -90,6 +101,44 @@ class UserVote{
     pressButton(button){
         this.saveVote(button.id);
     }
+
+
+
+    configureWebSocket() {
+        const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+        this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+        this.socket.onopen = (event) => {
+          this.displayMsg('system', 'guesser', 'connected');
+        };
+        this.socket.onclose = (event) => {
+          this.displayMsg('system', 'guesser', 'disconnected');
+        };
+        this.socket.onmessage = async (event) => {
+          const msg = JSON.parse(await event.data.text());
+          if (msg.type === WrongVote) {
+            this.displayMsg('player', msg.from, `guessed incorrectly!`);
+          } else if (msg.type === RightVote) {
+            this.displayMsg('player', msg.from, `guessed correctly!`);
+          }else if(msg.type === DupeVote){
+            this.displayMsg('player', msg.from, 'tried to vote again!')
+          }
+        };
+      }
+    
+      displayMsg(cls, from, msg) {
+        const chatText = document.querySelector('#player-messages');
+        chatText.innerHTML =
+          `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div>` + chatText.innerHTML;
+      }
+    
+      broadcastEvent(from, type, value) {
+        const event = {
+          from: from,
+          type: type,
+          value: value,
+        };
+        this.socket.send(JSON.stringify(event));
+      }
 }
 const userVote = new UserVote();
 
